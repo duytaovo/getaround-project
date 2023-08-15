6709240a1978f04c1349eab37a6026c9567d28ac
duytaovo <duytaovo@gmail.com>
2023-08-15 15:25:15 +0700

    update

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -1,9 +1,9 @@
 import http, { http_auth } from 'src/utils/http'
 
 const authApi = {
-  login(data: any) {
-    return http_auth.post('auth/admin', data)
-  }
+  // login(data: any) {
+  //   return http_auth.post('auth/admin', data)
+  // }
 }
 
 export default authApi
